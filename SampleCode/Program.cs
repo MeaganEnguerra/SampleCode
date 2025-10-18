0af73002413d469d585cefd142a3332b1cba9a38
Meagan Enguerra <meagansilvano@gmail.com>
2025-10-18 13:44:49 +0800

    inputted name

--- SampleCode/Program.cs
+++ SampleCode/Program.cs
@@ -9,3 +9,6 @@ Console.WriteLine("sample 4s");
 
 Console.WriteLine("The only thing we have to fear is fear itself\" by Franklin D. Roosevelt,");
 Console.WriteLine("The only thing we have to fear is fear itself\" by Franklin D. Roosevelt,");
+
+
+Console.WriteLine("My name is Meagan Enguerra");
