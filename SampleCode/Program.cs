2ca0ff8d135d81d492fdfb8ebce5969b3eec9310
Meagan Enguerra <meagansilvano@gmail.com>
2025-10-18 13:35:29 +0800

    1st commit

--- SampleCode/Program.cs
+++ SampleCode/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("This is a sample code for testing the automated task!");
 
-Console.WriteLine("sample");
+Console.WriteLine("sample 1");
 
 Console.WriteLine("sample 2");
 Console.WriteLine("sample 3");
